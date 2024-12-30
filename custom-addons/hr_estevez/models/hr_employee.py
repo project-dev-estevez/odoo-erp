66d4862361f61e20d497fe885150a66cb7f6a47e
Alejandro Gómez <alejandrogomez2339@gmail.com>
2024-12-29 21:49:10 -0500

    Employees updated

--- custom-addons/hr_estevez/models/hr_employee.py
+++ custom-addons/hr_estevez/models/hr_employee.py
@@ -1,18 +1,33 @@
 from odoo import api, models, fields
+from datetime import date
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-
+    # Primera Columna en la Vista de Empleados
     names = fields.Char(string='Nombres', required=True)
     last_name = fields.Char(string='Apellido Paterno', required=True)
     mother_last_name = fields.Char(string='Apellido Materno', required=True)
+    employee_number = fields.Char(string='Número de Empleado')
+    project = fields.Char(string='Proyecto')
+
+    # Segunda Columna en la Vista de Empleados 
+    company_id = fields.Many2one('res.company', string='Company', compute='_compute_company', store=True, readonly=True)
     direction_id = fields.Many2one('hr.direction', string='Dirección')
     area_id = fields.Many2one('hr.area', string='Area')
+
+    # Información de Trabajo
+    imss_registration_date = fields.Date(string='Fecha de Alta en IMSS')
     payment_type = fields.Selection([
         ('weekly', 'Semanal'),
         ('biweekly', 'Quincenal'),
-    ], string='Tipo de Pago', required=True)
+    ], string='Tipo de Pago')
+    payroll_type = fields.Selection([
+        ('cash', 'Efectivo'),
+        ('mixed', 'Mixto'),
+        ('imss', 'IMSS'),
+    ], string='Tipo de Nómina')
+
 
     rfc = fields.Char(string='RFC')
     curp = fields.Char(string='CURP')
@@ -23,10 +38,11 @@ class HrEmployee(models.Model):
     private_colonia = fields.Char(string="Colonia")
     fiscal_zip = fields.Char(string="Fiscal ZIP")
 
-    work_phone = fields.Char(string='Work Phone', compute=False, store=False)
+    work_phone = fields.Char(string='Work Phone', compute=False)
     coach_id = fields.Many2one('hr.employee', string='Instructor', compute=False, store=False)
 
     name = fields.Char(string='Nombre Completo', compute='_compute_full_name', store=True, readonly=True)
+    age = fields.Integer(string='Edad', compute='_compute_age')
 
     @api.depends('names', 'last_name', 'mother_last_name')
     def _compute_full_name(self):
@@ -70,3 +86,12 @@ class HrEmployee(models.Model):
 
         res = super(HrEmployee, self).write(vals)
         return res
+    
+    @api.depends('birthday')
+    def _compute_age(self):
+        for record in self:
+            if record.birthday:
+                today = date.today()
+                record.age = today.year - record.birthday.year - ((today.month, today.day) < (record.birthday.month, record.birthday.day))
+            else:
+                record.age = 0
